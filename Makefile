479720ea8b2359016ba7efbdde75bf1b1439d967
prok05 <prok2905@gmail.com>
2024-11-25 19:23:48 +0300

    added start-dev for make

--- Makefile
+++ Makefile
@@ -4,6 +4,9 @@ run: build
 migration:
 	@migrate create -ext sql -dir migrate/migrations $(filter-out $@,$(MAKECMDGOALS))
 
+start-dev:
+	@go run cmd/main.go
+
 migrate-up:
 	@go run migrate/main.go up
 
--- config/env.go
+++ config/env.go
@@ -37,7 +37,7 @@ func initConfig() Config {
 	//envPath := filepath.Join(workDir, ".env")
 	//log.Println(envPath)
 
-	err := godotenv.Load("../.env")
+	err := godotenv.Load(".env")
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
